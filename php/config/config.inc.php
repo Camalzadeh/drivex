5cf53558a98b781d4e884df27f8f6127a9c984ac
Humbat Jamalov <143912059+Camalzadeh@users.noreply.github.com>
2025-12-25 14:01:49 +0400

    feat: add initial database and environment configuration

--- php/config/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'db_host' => 'mysql-jamalzadeh.alwaysdata.net',
-    'db_name' => 'jamalzadeh_db',
-    'db_user' => '443345',
-    'db_pass' => 'humbet2006',
-    'environment' => 'production' 
-];
